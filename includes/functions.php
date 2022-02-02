04aef8d59946c2e198dedc13db03e6959e760420
andrkrtv <legendary3501@gmail.com>
2022-02-02 12:25:54 +0300

    debug, add_user
    добавление дебага и новых функций

--- includes/functions.php
+++ includes/functions.php
@@ -1,10 +1,24 @@
 <?php
 include_once "config.php";
 
+function  debug($var, $stop = false){
+echo "<pre>";
+print_r($var);
+echo "</pre>";
+if ($stop) die;
+}
+
 function get_url($page ='') {
 	return HOST . "/$page";
 }
 
+function get_page_title($title=''){
+if (!empty($title)) {
+	return SITE_NAME  . " - $title";
+	}else{
+		return SITE_NAME;
+}
+}
 function db() {
 	try {
 		return new PDO("mysql:host=". DB_HOST. ";dbname=". DB_NAME . ";charset=utf8", DB_USER, DB_PASS,
@@ -29,11 +43,28 @@ function db_query($sql, $exec = false) {
 
 function get_posts($user_id = 0) {
 	if ($user_id > 0) {
-		return db_query("SELECT posts.*, users.name, users.login, users.avatar FROM `posts` JOIN `users` ON users.id = posts.user_id WHERE posts.user_id = $user_id;");
+		return db_query("SELECT posts.*, users.name, users.login, users.avatar FROM `posts` JOIN `users` ON users.id = posts.user_id WHERE posts.user_id = $user_id;") ->fetchAll();;
 	} else {
-		return db_query("SELECT posts.*, users.name, users.login, users.avatar FROM `posts` JOIN `users` ON users.id = posts.user_id;");
+		return db_query("SELECT posts.*, users.name, users.login, users.avatar FROM `posts` JOIN `users` ON users.id = posts.user_id;") ->fetchAll();
+	}
 	 function get_user_info($login){
 		return db_query("SELECT * FROM  `user` WHERE `login`  = '$login';")->fetch();
 
 	}
+}
+
+function add_user($login, $pass){
+	$login = trim($login);
+	$name = ucfirst($login);
+	$password = password_hash($pass,PASSWROD_DEFAULT);
+	return db_query ("INSERT INTO `users` (`id`, `login`, `pass`, `name`) VALUES (NULL, '$login', '$password', '$name');",true);
+
+}
+function register_user($auth_data){
+debug($auth_data, true);
+
+}
+
+function login ($auth_data){
+
 }
