91008ef6295e9f4a655f2828f1f29e1802b7a0d0
andrkrtv <legendary3501@gmail.com>
2022-02-02 12:34:24 +0300

    Присваивание имён

--- includes/register_form.php
+++ includes/register_form.php
@@ -3,9 +3,6 @@
 			<div class="tweet-form__error">Что-то пошло не так</div>
 			<form class="tweet-form" action="<?php echo get_url('include/sign_up.php');?>" method = "post"> 
 				<div class="tweet-form__wrapper_inputs">
-					<input type="text" class="tweet-form__input" placeholder="Логин" required>
-					<input type="password" class="tweet-form__input" placeholder="Пароль" required>
-					<input type="password" class="tweet-form__input" placeholder="Пароль повторно" required>
 				</div>
 				<div class="tweet-form__btns_center">
 					<button class="tweet-form__btn_center" type="submit">Зарегистрироваться</button>
